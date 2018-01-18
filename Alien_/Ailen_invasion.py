1c860304f0ce20c5ed8c92877559a217b988c6da
式子墨 <q17010112@njupt.edu.cn>
2018-01-18 18:58:24 +0800

    1.18 18:58

--- Alien_/Ailen_invasion.py
+++ Alien_/Ailen_invasion.py
@@ -19,11 +19,12 @@ def run_game():
     bg_color = (230,230,230)
 
     #创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
 
     #开始游戏的主循环
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings,screen,ship)
 
 run_game()
